30f6bfc70c8c1388e521a4ebfd9bc1be6ecc8b89
Jon Kurinsky <jkurinsky@gmail.com>
2019-03-26 08:32:13 -0500

    add new student and new campus links

--- app/components/AllCampuses.js
+++ app/components/AllCampuses.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import SmallCampusCard from './SmallCampusCard';
+import { Link } from 'react-router-dom';
 
 export const AllCampuses = props => {
   return (
-    <div className="small-card-container campus">
-      {props.campuses.length
-        ? props.campuses.map(campus => {
-            return <SmallCampusCard campus={campus} key={campus.id} />;
-          })
-        : 'No Campuses'}
+    <div>
+      <div className="sub-nav">
+        <span className="nav-link">
+          [<Link to="/campuses/add">Add New Campus</Link>]
+        </span>
+      </div>
+      <div className="small-card-container campus">
+        {props.campuses.length
+          ? props.campuses.map(campus => {
+              return <SmallCampusCard campus={campus} key={campus.id} />;
+            })
+          : 'No Campuses'}
+      </div>
     </div>
   );
 };
--- app/components/AllStudents.js
+++ app/components/AllStudents.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import SmallStudentCard from './SmallStudentCard';
+import { Link } from 'react-router-dom';
 
 export const AllStudents = props => {
   return (
-    <div className="small-card-container student">
-      {props.students.length
-        ? props.students.map(student => {
-            return <SmallStudentCard student={student} key={student.id} />;
-          })
-        : 'No Students'}
+    <div>
+      <div className="sub-nav">
+        <span className="nav-link">
+          [<Link to="/studnets/add">Add New Student</Link>]
+        </span>
+      </div>
+      <div className="small-card-container student">
+        {props.students.length
+          ? props.students.map(student => {
+              return <SmallStudentCard student={student} key={student.id} />;
+            })
+          : 'No Students'}
+      </div>
     </div>
   );
 };
