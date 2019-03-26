922f95e1377501815552dd734b6decb947ab1287
Jon Kurinsky <jkurinsky@gmail.com>
2019-03-25 21:43:38 -0500

    split out SmallCampusCard

--- app/components/AllCampuses.js
+++ app/components/AllCampuses.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import SmallCampusCard from './SmallCampusCard';
 
 export const AllCampuses = props => {
   return (
     <div className="small-card-container">
       {props.campuses.length
         ? props.campuses.map(campus => {
-            return (
-              <div key={campus.id} className="card">
-                <Link to={`/campuses/${campus.id}`}>
-                  <div className="card-label">{campus.name}</div>
-                  <img className="card-image" src={campus.imageUrl} />
-                </Link>
-                <div className="card-links-container">
-                  <span>
-                    [
-                    <Link to="/" className="edit">
-                      edit
-                    </Link>
-                    ]
-                  </span>
-                  <span>
-                    [
-                    <Link to="/" className="delete">
-                      delete
-                    </Link>
-                    ]
-                  </span>
-                </div>
-              </div>
-            );
+            return <SmallCampusCard campus={campus} key={campus.id} />;
           })
         : 'No Campuses'}
     </div>
--- app/components/SmallCampusCard.js
+++ app/components/SmallCampusCard.js
@@ -2,7 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const SmallCampusCard = props => {
-  return <div />;
+  const campus = props.campus;
+  return (
+    <div key={campus.id} className="card">
+      <Link to={`/campuses/${campus.id}`}>
+        <div className="card-label">{campus.name}</div>
+        <img className="card-image" src={campus.imageUrl} />
+      </Link>
+      <div className="card-links-container">
+        <span>
+          [
+          <Link to="/" className="edit">
+            edit
+          </Link>
+          ]
+        </span>
+        <span>
+          [
+          <Link to="/" className="delete">
+            delete
+          </Link>
+          ]
+        </span>
+      </div>
+    </div>
+  );
 };
 
 export default SmallCampusCard;
