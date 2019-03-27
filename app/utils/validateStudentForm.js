0b22cf40b5ccd9670c5d4932a13fe81b0c3e4e10
Jon Kurinsky <jkurinsky@gmail.com>
2019-03-27 13:27:28 -0500

    improve visibility of form validation messages

--- app/utils/validateStudentForm.js
+++ app/utils/validateStudentForm.js
@@ -1,22 +1,22 @@
 const validateStudentForm = state => {
   let validationMessageArray = [];
-  if (this.state.firstName === '') {
+  if (state.firstName === '') {
     validationMessageArray.push('First name cannot be blank.');
   }
-  if (this.state.lastName === '') {
+  if (state.lastName === '') {
     validationMessageArray.push('Last name cannot be blank.');
   }
-  if (this.state.email === '') {
+  if (state.email === '') {
     validationMessageArray.push('Email cannot be blank.');
   }
   const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if (!re.test(this.state.email.toLowerCase())) {
+  if (!re.test(state.email.toLowerCase())) {
     validationMessageArray.push('Email must be valid.');
   }
-  if (this.state.gpa === '') {
+  if (state.gpa === '') {
     validationMessageArray.push('GPA cannot be blank.');
   }
-  if (this.state.gpa < 0 || this.state.gpa > 4) {
+  if (state.gpa < 0 || state.gpa > 4) {
     validationMessageArray.push('GPA must be between 0.0 and 4.0.');
   }
   return validationMessageArray;
